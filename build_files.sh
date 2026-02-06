9344df7913202f5436b41ffcde66193d437fc6cc
rusysh22 <rusydani.sh@gmail.com>
2026-02-06 14:57:26 +0700

    build: Remove database migration commands from build script.

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,5 @@
 echo "Building the project..."
 # pip install is handled by Vercel automatically
 
-
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
 echo "Collect Static..."
 python manage.py collectstatic --noinput --clear
