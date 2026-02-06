869889f7f4350d9d7f1e1f45be64ba4e10c0c293
rusysh22 <rusydani.sh@gmail.com>
2026-02-06 17:45:05 +0700

    Fix static files build process

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
 echo "Building the project..."
-# pip install is handled by Vercel automatically
+python3.12 -m pip install -r requirements.txt
 
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
